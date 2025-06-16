2753b1d118cf038eb5caacdfe2d6ef6aa6976b18
adamcaak <ladamczak1997@gmail.com>
2025-06-16 23:08:20 +0200

    Create Button

--- Calendar/ContentView.swift
+++ Calendar/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = CalendarViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 25) {
+                HStack {
+                    Button(action: { viewModel.moveMonth(by: -1)}) {
+                        Image(systemName: "chevron.left")
+                            .font(.title3)
+                            .foregroundColor(.white)
+                            .frame(width: 44, height: 44)
+                            .background(viewModel.accentGradient)
+                            .clipShape(Circle())
+                            .shadow(color: viewModel.primaryColor, radius: 4, x: 0, y: 2)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
